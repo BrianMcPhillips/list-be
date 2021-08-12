a71df9aa75ca997dc51dbb1ff5ae5a9774b47dbb
Brian McPhillips <B.McPhillips89@gmail.com>
2021-08-11 20:59:10 -0700

    Renamed seed data and new table

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,11 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE discs (
                     id SERIAL PRIMARY KEY NOT NULL,
+                    brand VARCHAR(120) NOT NULL,
                     name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    speed INTEGER NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
---
+++ data/discs.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    brand: 'Innova',
+    name: 'Leopard',
+    speed: 6
+  },
+  {
+    brand: 'Innova',
+    name: 'Beast',
+    speed: 10
+  },
+  {
+    brand: 'Innova',
+    name: 'Tee-Bird',
+    speed: 7
+  },
+  {
+    brand: 'Innova',
+    name: 'Valkyrie',
+    speed: 9
+  },
+  {
+    brand: 'Innova',
+    name: 'VROC',
+    speed: 4
+  }
+];
